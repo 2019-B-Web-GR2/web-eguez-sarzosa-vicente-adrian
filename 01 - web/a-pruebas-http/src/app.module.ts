c7798f8f90a52897386c87ab4b0ea5a1ac2377fd
Adrian Eguez <eadepto@hotmail.com>
2019-12-12 15:57:58 -0500

    modulo usuario+

--- 01 - web/a-pruebas-http/src/app.module.ts
+++ 01 - web/a-pruebas-http/src/app.module.ts
@@ -1,27 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsuarioEntity} from "./usuario/usuario.entity";
+import {UsuarioModule} from "./usuario/usuario.module";
 
 @Module({
-  imports: [
-      TypeOrmModule.forRoot(
-          {
-                  type: 'mysql',
-                  host: 'localhost',
-                  port: 32775,
-                  username: 'adrian',
-                  password: '1234',
-                  database: 'web',
-                  entities: [
-                      UsuarioEntity
-                  ],
-                  synchronize: true, // Crear -> true , Conectar -> false
-          }
-      )
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UsuarioModule,
+        TypeOrmModule.forRoot(
+            {
+                name: 'default', // Nombre cadena de Conex.
+                type: 'mysql',
+                host: '172.31.108.148',
+                port: 32769,
+                username: 'LazaMH',
+                password: '1234',
+                database: 'Prueba',
+                entities: [
+                    UsuarioEntity
+                ],
+                synchronize: true, // Crear -> true , Conectar -> false
+            }
+        ),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
